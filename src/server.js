eb4509be93165f20c24e1ae7c206a4f87e387437
gavinzhou <gavin.zhou@gmail.com>
2016-12-03 01:26:39 +0000

    use https

--- src/server.js
+++ src/server.js
@@ -2,6 +2,8 @@ import Express from 'express'
 import cors from 'cors'
 import bodyParser from 'body-parser'
 import { customer, webhook } from './handlers'
+import https from 'https'
+import pem from 'pem'
 
 const PORT = process.env.PORT || 5001
 
@@ -13,6 +15,8 @@ app.get('/', (req, res) => res.send('Hello'))
 app.post('/customers', customer.create)
 app.post('/webhooks', webhook.handle)
 
-app.listen(PORT, () => {
-  console.log(`app listening on port ${PORT}!`) // eslint-disable-line no-console
+pem.createCertificate({days:365, selfSigned:true}, function(err, keys){
+  https.createServer({key: keys.serviceKey, cert: keys.certificate}, app).listen(PORT, () => {
+    console.log(`app listening on port ${PORT}!`) // eslint-disable-line no-console
+  })
 })
