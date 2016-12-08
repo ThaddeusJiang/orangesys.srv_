0c3fd6b88f3df84e7858869a6a2219280dded6f1
gaishimo <gaithimo@gmail.com>
2016-12-08 11:00:30 +0900

    Add prefix to api paths

--- src/server.js
+++ src/server.js
@@ -4,14 +4,14 @@ import bodyParser from 'body-parser'
 import { customer, webhook } from './handlers'
 
 const PORT = process.env.PORT || 5001
+const API_PREFIX = '/stripe'
 
 const app = new Express()
 app.use(cors())
 app.use(bodyParser.json())
 
-app.get('/', (req, res) => res.send('Hello'))
-app.post('/customers', customer.create)
-app.post('/webhooks', webhook.handle)
+app.post(`${API_PREFIX}/customers`, customer.create)
+app.post(`${API_PREFIX}/webhooks`, webhook.handle)
 
 app.listen(PORT, () => {
   console.log(`app listening on port ${PORT}!`) // eslint-disable-line no-console
